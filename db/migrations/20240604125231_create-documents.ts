7c6427c13296cfd54267235be1f0bb4aa30281b1
rbbaptista <rodolfobrunelli@gmail.com>
2024-06-04 23:56:25 -0300

    first table

--- db/migrations/20240604125231_create-documents.ts
+++ db/migrations/20240604125231_create-documents.ts
@@ -1,10 +1,10 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+	await knex.schema.createTable("transactions", (table) => {
+		table.uuid("id").primary();
+		table.text("title").notNullable();
+	});
 }
 
-
-export async function down(knex: Knex): Promise<void> {
-}
-
+export async function down(knex: Knex): Promise<void> {}
